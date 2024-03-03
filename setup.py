88cd8aa8e474dcfda1484814592b2a94068f9c1a
fynnb <fynn.bremser@gmx.de>
2024-03-03 23:45:25 +0100

    Version 1.1.0
    New Features:
    - Enum Support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyserial',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/fynnbrem/pyserial',
     author='Fynn Bremser',
     packages=find_packages(),
